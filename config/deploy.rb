d5d605750f484ec8e08b9b382258f781e10fe7ea
Ning Yap <ning.yap@gmail.com>
2013-06-27 15:52:28 -0400

    capify

---
+++ config/deploy.rb
@@ -0,0 +1,31 @@
+set :application, "Playlister-YouTube"
+set :repository,  "git@github.com:ningbit/playlister-sinatra.git"
+
+set :user, 'ningbit'
+set :deploy_to, "/home/#{ user }/#{ application }"
+set :use_sudo, false
+
+set :scm, :git
+
+default_run_options[:pty] = true
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "192.241.131.188"                          # Your HTTP server, Apache/etc
+role :app, "192.241.131.188"                          # This may be the same as your `Web` server
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
